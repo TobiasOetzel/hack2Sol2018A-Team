152f324f0898f77fb7bfe75598df553446cd08a1
Sascha Scholz <sa.scholz@sap.com>
2018-06-21 18:01:37 +0200

    add change bulb api stub

--- com.sap.lys/server.js
+++ com.sap.lys/server.js
@@ -2,8 +2,33 @@ var approuter = require('@sap/approuter');
 
 var ar = approuter();
 
-ar.beforeRequestHandler.use(function myMiddleware(req, res, next) {
-    next();
+ar.beforeRequestHandler.use("/api/bulb/change", function changeBulbHandler(req, res, next) {
+    if (req.method !== "POST") {
+        httpMethodNotAllowed(res);
+    } else {
+        changeBulb(req,res);
+    }
 });
+
+function httpMethodNotAllowed(res) {
+    res.statusCode = 405;
+    res.end("Method not allowed");
+}
+
+function httpBadMethod(res) {
+    res.statusCode = 400;
+    res.end("Bad request");
+}
+
+function changeBulb(req, res) {
+    var thing = req.url.split("/")[1];
+    if (!thing) {
+        httpBadMethod(res);
+    }
+
+    req.url.split("/")
+    res.statusCode = 200;
+    res.end();
+}
 
 ar.start();
